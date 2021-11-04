b0e04db89cf6fab9a955cea695233ca771ce9d79
EnterGithubUsernam3 <93727477+EnterGithubUsernam3@users.noreply.github.com>
2021-11-04 18:56:22 +0200

    Update calculatorExce.js

--- calculatorExce.js
+++ calculatorExce.js
@@ -122,36 +122,3 @@ function subtraction (oneNumber,secondNumber,anotherOperator)
 
 
 Calculator(userInput1, userInput2, operator);
-// function calculator(a,b,sym)
-// {
-// switch (sym)
-// {
-// case "+":
-// return a+b;
-// break;
-// case "-":
-// return a-b;
-// break;
-// case "*":
-// return a*b;
-// break;
-// case "/":
-// if (b!=0)
-// {
-// return a/b;
-// }
-// else
-// {
-// return "I cannot perform the division by zero";
-// }
-// break;
-// default:
-// return "wrong operation!";
-// break;
-// }
-// }
-// var num1=+prompt("Give me number 1");
-// var symbol=prompt ("Give me the operation");
-// var num2=+prompt("Give me number 2");
-// result= calculator(num1,num2,symbol);
-// console.log(result);
